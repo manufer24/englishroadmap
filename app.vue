a6135e08f3982d2e9fb1e28fb1cf62003211b68c
manufer24 <manuelferg13@gmail.com>
2023-11-04 15:10:23 -0400

    picture updated

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@ useSeoMeta({
   ogTitle: 'English Roadmap',
   description: 'Hojas de ruta ordenadas, guias y contenido educaional que ayuden a orientar a las personas a la hora de elegir un camino y guiar su aprendizaje del idioma inglés.',
   ogDescription: 'Hojas de ruta ordenadas, guias y contenido educaional que ayuden a orientar a las personas a la hora de elegir un camino y guiar su aprendizaje del idioma inglés.',
-  ogImage: 'https://englishroadmap.vercel.app/english-roadmap-meta.png',
+  ogImage: 'https://englishroadmap.vercel.app/public/english-roadmap-meta.png',
   twitterCard: 'summary_large_image',
 })
 </script>
