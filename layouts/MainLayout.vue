e0cdfdf2ff35ca3613bd0b95919ac1b5ea85a918
manufer24 <manuelferg13@gmail.com>
2024-01-21 13:47:31 -0400

    update header section

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,24 +1,30 @@
 <template>
-    <header class="container mx-auto fixed navbar flex justify-between lg:px-28 border-b border-neutral-content bg-base-200 z-40">
-        <nuxt-link 
-            class="font-bold text-3xl text-primary"
-            to="/"
-        >
-            ER
-        </nuxt-link>
-        <nav class="flex justify-end ">
-            <primaryMenu />
-            <div class="dropdown dropdown-bottom dropdown-end mr-3">
-            <div tabindex="0" role="button" class="btn m-1 bg-primary text-primary-content hover:bg-primary-content hover:text-primary">Menu</div>
-                <ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 border-2 border-primary">
-                    <li><nuxt-link class="hover:bg-primary-content hover:text-primary" to="/FAQs">FAQS</nuxt-link></li>
-                    <li><nuxt-link class="hover:bg-primary-content hover:text-primary" to="/contributors">Colaboradores</nuxt-link></li>
-                </ul>
-            </div>
+
+    <header class="container py-2 px-1">
+        <nav class="navbar flex justify-between rounded-2xl shadow-xl bg-base-100">
+            <button class="btn btn-primary rounded-xl">
+                <nuxt-link 
+                    class="font-bold text-xl"
+                    to="/"
+                >
+                    ER
+                </nuxt-link>
+            </button>
+
+            <section class="space-x-2">
+                <primaryMenu />
+                <button class="dropdown dropdown-bottom dropdown-end mr-3">
+                    <div tabindex="0" role="button" class="btn btn-primary">Menu</div>
+                        <ul class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52 mt-1 space-y-1">
+                            <li><nuxt-link class="btn btn-sm btn-block btn-ghost justify-start" to="/FAQs">FAQS</nuxt-link></li>
+                            <li><nuxt-link class="btn btn-sm btn-block btn-ghost justify-start" to="/contributors">Colaboradores</nuxt-link></li>
+                        </ul>
+                </button>
+            </section>
         </nav>
     </header>
 
-    <main class="pt-20">
+    <main class="container px-3">
         <slot />
     </main>
 </template>
