366ff41eb27afe2b8fe2be86ec22dc62de569f81
manufer24 <manuelferg13@gmail.com>
2024-02-26 11:20:04 -0400

    clean comments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode'
   ],
   colorMode: {
-    preference: 'system', // default theme
-    dataValue: 'theme', // activate data-theme in <html> tag
+    preference: 'system',
+    dataValue: 'theme', 
     classSuffix: '',
   },
   css: ['~/assets/css/main.css'],
