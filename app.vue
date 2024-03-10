8b21d1c19654f759cb7e2a448f76855771d83055
manufer24 <manuelferg13@gmail.com>
2024-03-10 12:31:05 -0400

    remove SEO script

--- app.vue
+++ app.vue
@@ -4,12 +4,3 @@
     <NuxtPage />
   </div>
 </template>
-<script setup lang="ts">
-useSeoMeta({
-  title: 'English Roadmap',
-  ogTitle: 'English Roadmap',
-  description: 'Hojas de ruta ordenadas, guias y contenido educaional que ayuden a orientar a las personas a la hora de elegir un camino y guiar su aprendizaje del idioma inglés.',
-  ogDescription: 'Hojas de ruta ordenadas, guias y contenido educaional que ayuden a orientar a las personas a la hora de elegir un camino y guiar su aprendizaje del idioma inglés.',
-  twitterCard: 'summary_large_image',
-})
-</script>
