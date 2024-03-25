27f8c863bf06ca750dd68dfc9ede2678df653c2c
manufer24 <manuelferg13@gmail.com>
2024-03-25 12:47:09 -0400

    fix text and add accessibility tags

--- components/BasicGoHomeBtn.vue
+++ components/BasicGoHomeBtn.vue
@@ -1,7 +1,8 @@
 <template>
         <nuxt-link
-        class="container mx-auto mb-10 flex items-center justify-center w-16 btn btn-active bg-primary text-primary-content hover:bg-neutral hover:text-primary"
+        class="container mx-auto mb-10 text-base flex items-center justify-center btn btn-active btn-wide leading-tight tracking-wider font-Poppins bg-primary text-primary-content hover:bg-neutral hover:text-primary"
             to="/aprender"
+            title="Volver"
         >
             volver
         </nuxt-link>
--- components/GoHomeBtn.vue
+++ components/GoHomeBtn.vue
@@ -1,7 +1,8 @@
 <template>
         <nuxt-link
-        class="container mx-auto my-10 flex items-center justify-center w-16 btn btn-active bg-primary text-primary-content hover:bg-neutral hover:text-primary"
+            class=" text-base leading-tight tracking-wider font-Poppins container mx-auto my-10 flex items-center justify-center btn btn-active btn-wide bg-primary text-primary-content hover:bg-neutral hover:text-primary"
             to="/"
+            title="Volver al inicio"
         >
             volver
         </nuxt-link>
